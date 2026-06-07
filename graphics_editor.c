5211110ad490c29a5ce6d43460a34ab8e1631ccb
snigdhamahesh <Snigdhamahesh693@gmail.com>
2026-06-07 20:49:55 +0530

    Day 4 - Line drawing added
    Added a function to draw horizontal lines on the canvas.

--- graphics_editor.c
+++ graphics_editor.c
@@ -48,6 +48,18 @@ void displayCanvas()
         printf("\n");
     }
 }
+void drawLine(int row1, int col1, int row2, int col2)
+{
+    int i;
+
+    if(row1 == row2)
+    {
+        for(i = col1; i <= row2; i++)
+        {
+            canvas[row1][i] = '*';
+        }
+    }
+}
 int main()
 {
     int choice;
@@ -60,7 +72,8 @@ int main()
         printf("\n\n===== 2D Graphics Editor =====\n");
         printf("1. Display Canvas\n");
         printf("2. Draw Rectangle\n");
-        printf("3. Exit\n");
+        printf("3. Draw Line\n");
+        printf("4. Exit\n");
 
         printf("Enter your choice: ");
         scanf("%d", &choice);
