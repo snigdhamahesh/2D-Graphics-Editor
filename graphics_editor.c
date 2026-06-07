1da48f905ebc5bc73ad54d7062e993f3ce828675
snigdhamahesh <Snigdhamahesh693@gmail.com>
2026-06-07 20:42:00 +0530

    Day 3 - Menu system added

--- graphics_editor.c
+++ graphics_editor.c
@@ -48,14 +48,45 @@ void displayCanvas()
         printf("\n");
     }
 }
-
 int main()
 {
+    int choice;
+    int row, col, width, height;
+
     initCanvas();
 
-    drawRectangle(2, 5, 10, 5);
+    while(1)
+    {
+        printf("\n\n===== 2D Graphics Editor =====\n");
+        printf("1. Display Canvas\n");
+        printf("2. Draw Rectangle\n");
+        printf("3. Exit\n");
 
-    displayCanvas();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    return 0;
+        switch(choice)
+        {
+            case 1:
+                displayCanvas();
+                break;
+
+            case 2:
+                printf("Enter row column width height: ");
+                scanf("%d%d%d%d", &row, &col, &width, &height);
+
+                drawRectangle(row, col, width, height);
+
+                printf("Rectangle drawn successfully!\n");
+                break;
+
+            case 3:
+                printf("Goodbye!\n");
+                return 0;
+
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
 }
+
